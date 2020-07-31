ef75e074032be1206633ce7c7bb6de7cece1f1e6
Caia Chuang <caiachung@gmail.com>
2020-07-31 00:55:08 +0000

    this cat test is annoying

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,22 @@
 const {API_QUERY,} = require('./constants');
 const request = require('request');
-const inputQuery = process.argv.slice(2);
 
-request(API_QUERY + inputQuery, (error, body) => {
-  const data = JSON.parse(body)[0];
- 
-  switch (data) {
-  case error:
-    console.log(error);
-    break;
-  case undefined:
-    console.log(`Breed ${inputQuery} not found!`);
-    break;
-  default:
-    console.log(`Your search result: \n` + data.description);
+const fetchBreedDescription = function(breedName, callback) {
+  if (!breedName) {
+    return callback('You didn\'t input any breed...', null);
   }
-});
+
+  request(API_QUERY + breedName, (error, response, body) => {
+    let data = JSON.parse(body)[0];
+    if (error) {
+      return callback(error, null);
+    } else if (data === undefined) {
+      return callback('Breed not found!', null);
+    } else if (data) {
+      return callback(null, data.description);
+    }
+  });
+};
+
+
+module.exports = {fetchBreedDescription};
